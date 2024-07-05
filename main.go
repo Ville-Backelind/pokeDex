0c5484487c7d4e150f63cdca90eca18c29f6c331
Ville Backelind <villebergbackelind@gmail.com>
2024-07-05 21:31:13 +0200

    Update main.go
    Some CLI stuff

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    //"strings"
-    "os"
+	"bufio"
+	"fmt"
+
+	//"strings"
+	"os"
 )
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    fmt.Println(scanner.Text())
+off:
+	for {
+		fmt.Printf("PokeDex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+
+		switch s := scanner.Text(); s {
+		case "":
+		case "exit":
+			break off
+		default:
+			fmt.Printf("'%v', is an unknown command. \n\n- Type help for help page. \n\n- Type exit to quite the program.\n\n", s)
+		}
+	}
+
 }
